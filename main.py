88d9e9da4085c537739637e7bdaf0a6146d59c4b
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-04-25 20:14:25 +0300

    add numpy based game map and its drawing

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import sys
 import pygame
+import numpy as np
+from enum import Enum
 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+
+class CellType(Enum):
+    FLOOR = 0
+    WALL = 1
+    PLAYER = 2
+    DIAMOND = 3
+
+
+cell_type_colors = {
+    CellType.FLOOR: (200, 200, 200),
+    CellType.WALL: (0, 200, 0),
+    CellType.PLAYER: (200, 0, 0),
+    CellType.DIAMOND: (0, 0, 200),
+}
+
+
+game_map = np.array([
+    [CellType.WALL, CellType.WALL,   CellType.WALL,  CellType.WALL,   CellType.WALL],
+    [CellType.WALL, CellType.FLOOR,  CellType.FLOOR, CellType.FLOOR,  CellType.WALL],
+    [CellType.WALL, CellType.PLAYER, CellType.FLOOR, CellType.FLOOR,  CellType.WALL],
+    [CellType.WALL, CellType.FLOOR,  CellType.FLOOR, CellType.DIAMOND, CellType.WALL],
+    [CellType.WALL, CellType.WALL,   CellType.WALL,  CellType.WALL,   CellType.WALL],
+])
+
+
+CELL_SIZE = 50
+WINDOW_HEIGHT, WINDOW_WIDTH = tuple(x * CELL_SIZE for x in game_map.shape)
 
 
 def main():
     pygame.init()
     drawing_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    BLACK = (0, 0, 0)
     drawing_surface.fill(BLACK)
 
     while True:
@@ -23,11 +49,12 @@ def main():
 
 
 def draw_grid(drawing_surface):
-    block_size = 20  # Set the size of the grid block
-    for x in range(0, WINDOW_WIDTH, block_size):
-        for y in range(0, WINDOW_HEIGHT, block_size):
-            rect = pygame.Rect(x, y, block_size, block_size)
-            pygame.draw.rect(drawing_surface, WHITE, rect, 1)
+    for i, row in enumerate(game_map):
+        y = i * CELL_SIZE
+        for j, cell in enumerate(row):
+            x = j * CELL_SIZE
+            rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(drawing_surface, cell_type_colors[cell], rect)
 
 
 if __name__ == '__main__':
