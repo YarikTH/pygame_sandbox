2274fa920ba3be8d3d6dd3b8187aa5624ceb3594
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-04-25 21:06:51 +0300

    add player movement with keyboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 import numpy as np
+import glm
 from enum import Enum
 
 
@@ -11,11 +12,28 @@ class CellType(Enum):
     DIAMOND = 3
 
 
+class Direction(Enum):
+    NONE = -1
+    TOP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+
 cell_type_colors = {
     CellType.FLOOR: (200, 200, 200),
     CellType.WALL: (0, 200, 0),
     CellType.PLAYER: (200, 0, 0),
     CellType.DIAMOND: (0, 0, 200),
+}
+
+
+direction_offsets = {
+    Direction.NONE:  glm.vec2(0,  0),
+    Direction.TOP:   glm.vec2(-1, 0),
+    Direction.RIGHT: glm.vec2(0,  1),
+    Direction.DOWN:  glm.vec2(1,  0),
+    Direction.LEFT:  glm.vec2(0,  -1),
 }
 
 
@@ -30,15 +48,52 @@ game_map = np.array([
 
 CELL_SIZE = 50
 WINDOW_HEIGHT, WINDOW_WIDTH = tuple(x * CELL_SIZE for x in game_map.shape)
+player_pos = glm.vec2(2, 1)
+
+
+def game_map_element(pos):
+    return game_map[int(pos.x), int(pos.y)]
+
+
+def set_game_map_element(pos, value):
+    game_map[int(pos.x), int(pos.y)] = value
 
 
 def main():
+    global player_pos
+    global game_map
+
     pygame.init()
     drawing_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     BLACK = (0, 0, 0)
     drawing_surface.fill(BLACK)
 
-    while True:
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                direction = Direction.NONE
+                if event.key == pygame.K_w:
+                    direction = Direction.TOP
+                elif event.key == pygame.K_a:
+                    direction = Direction.LEFT
+                elif event.key == pygame.K_s:
+                    direction = Direction.DOWN
+                elif event.key == pygame.K_d:
+                    direction = Direction.RIGHT
+
+                if direction != Direction.NONE:
+                    player_target_pos = player_pos + direction_offsets[direction]
+                    player_target_cell = game_map_element(player_target_pos)
+                    if player_target_cell != CellType.WALL:
+                        set_game_map_element(player_pos, CellType.FLOOR)
+                        set_game_map_element(player_target_pos, CellType.PLAYER)
+                        player_pos = player_target_pos
+
         draw_grid(drawing_surface)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
